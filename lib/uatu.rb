aeb4e015d2c51aa15c1e40fdbcfc2cc89ac6ca4b
Jeremy David <ltouroumov@gmail.com>
2012-03-22 08:57:08 +0100

    UPPDATED: - Argument switches
    ADDED:
    - Interval between updates

--- lib/uatu.rb
+++ lib/uatu.rb
@@ -1,2 +1,80 @@
 require 'fssm'
 
+class Uatu
+
+	def self.run(options)
+		new(options).run!
+	end
+
+	def initialize(options)
+		@options = options
+	end
+
+	def run!
+		unless @options[:cmd] or @options[:script]
+			puts "'-s SCRIPT' or '-c CMD' arguments required !"
+			exit 1
+		end
+
+		puts "Watcher started"
+		@last_update = Time.now
+		FSSM.monitor(@options[:path]) do |mon|
+			mon.update &(trigger_block :update)
+			mon.delete &(trigger_block :delete)
+			mon.create &(trigger_block :create)
+		end
+	end
+
+private
+	
+	def trigger_block(evt)
+		proc { |base, rel|
+			trigger base, rel, evt
+		}
+	end
+
+	def trigger(base, rel, evt)
+		path = File.realpath(rel, base)
+		return if exclude? path
+		return unless time_elapsed?
+
+		puts "Changed %s" % [rel]
+		
+		if include? path
+			run_script path if @options[:script]
+			run_command path if @options[:cmd]
+			@last_update = Time.now
+		end
+	end
+
+	def exclude? (path)
+		return false unless @options[:exclude]
+		File.fnmatch(@options[:exclude], path)
+	end
+
+	def include? (path)
+		return true unless @options[:pattern]
+		File.fnmatch(@options[:pattern], path)
+	end
+
+	def time_elapsed?
+		return true unless @options[:time]
+
+		@last_update + @options[:time] <= Time.now
+	end
+
+	def run_script(change_path)
+		puts "Triggered '%s'" % [@options[:script]]
+		fork do
+			exec("%s %s" % [@options[:script], change_path])
+		end
+	end
+
+	def run_command(change_path)
+		puts "Triggered '%s'" % [@options[:cmd]]
+		fork do
+			exec(@options[:cmd] % [change_path])
+		end
+	end
+
+end
--- uatu.gemspec
+++ uatu.gemspec
@@ -1,13 +1,22 @@
 Gem::Specification.new do |spec|
   spec.name = 'uatu'
-  spec.version = '0.0.1'
-  spec.date = '21-03-2012'
+  spec.version = '0.5.0'
+  spec.date = '2012-03-21'
 
-  spec.authors = ['Jeremy David']
-  spec.emails = ['uatu@jeremydavid.ch']
+  spec.author = 'Jeremy David'
+  spec.email = 'uatu@jeremydavid.ch'
 
   spec.summary = %q{
+    Script trigger upon changes
+  }
+  spec.description = %q{
     A simple gem to trigger a script upon file system change
   }
   spec.homepage = 'http://github.com/ltouroumov/uatu'
+
+  spec.add_runtime_dependency 'fssm', '>= 0.2.8.1'
+
+  spec.files = ['bin/uatu', 'lib/uatu.rb']
+  spec.executables = ['uatu']
+  spec.require_paths = ['lib']
 end
